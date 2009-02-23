e1b411b3b6060e1cdddc7e7896d52bba55ccecde
Simon Josi <josi@puzzle.ch>
2009-02-23 13:54:28 +0000

    max 25 day vacations per year, even for > 100%

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -70,7 +70,7 @@ class Employment < ActiveRecord::Base
   end
   
   def vacations
-    period.length / 365.25 * VACATION_DAYS_PER_YEAR * [1.0, percentFactor].max
+    period.length / 365.25 * VACATION_DAYS_PER_YEAR * [1.0, percentFactor].min
   end 
     
   def musttime
