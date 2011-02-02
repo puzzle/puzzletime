1f7ad626afde893c70821bb630035ca00f71b583
Simon Fankhauser <fankhauser@puzzle.ch>
2011-02-02 00:40:03 +0000

    fixed some bug with the calculation of week load due to absences.

--- app/models/graphs/absence_planning_graph.rb
+++ app/models/graphs/absence_planning_graph.rb
@@ -32,17 +32,17 @@ class AbsencePlanningGraph
 
 private
   def add_start_stop_absence(absence)
-    add_to_cache(absence.timeString, absence.work_date)
+    add_to_cache(absence.timeString, absence.work_date, absence.hours*1)
   end
   
   def add_day_absence(absence)
-    add_to_cache(absence.timeString, absence.work_date)
+    add_to_cache(absence.timeString, absence.work_date, absence.hours*1)
   end
 
   def add_weekly_absence(absence)
     date = absence.work_date
     5.times do
-      add_to_cache(absence.timeString, date)
+      add_to_cache(absence.timeString, date, absence.hours/5)
       date = date.next
     end
   end
@@ -51,17 +51,21 @@ private
     dateFrom = Date.civil(absence.work_date.year, absence.work_date.month, 1)
     dateTo = Date.civil(dateFrom.year, dateFrom.month, -1)
     dateFrom.step(dateTo, 1) do |date|
-      add_to_cache(absence.timeString, date)
+      add_to_cache(absence.timeString, date, absence.hours/31)   # TODO: find number of work days in the month and divide by this number
     end
   end
   
-  def add_to_cache(label, date)
+  def add_to_cache(label, date, hours=0)
     cached = @cache[date]
     unless cached
       cached = DayOverview.new
       @cache[date] = cached
     end
-    cached.add(label)
+    if hours==0
+      cached.add(label)
+    else
+      cached.add(label, hours/8*10)
+    end
   end
 
 end
--- app/models/graphs/employee_overview_planning_graph.rb
+++ app/models/graphs/employee_overview_planning_graph.rb
@@ -47,8 +47,8 @@ private
     absence_graph.each_day do |date|
       absence = absence_graph.absence(date)
       if absence
-        add_to_cache(absence.label, date)
-        add_to_cache(absence.label, date)
+        add_to_cache(absence.label, date, absence.percent)
+        add_to_cache(absence.label, date, absence.percent)
         #TODO: implement real half day absences
       end
     end
