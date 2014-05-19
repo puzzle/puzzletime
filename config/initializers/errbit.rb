c79314e3e0b1d521099064791d199b2d055c6c4c
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-05-19 14:24:04 +0200

    add errbit and rpm configs

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+Airbrake.configure do |config|
+  # if no host is given, use airbrake only in test mode
+  config.test_mode   = ENV['RAILS_AIRBRAKE_HOST'].blank?
+
+  config.api_key     = ENV['RAILS_AIRBRAKE_API_KEY']
+  config.host        = ENV['RAILS_AIRBRAKE_HOST']
+  config.port        = (ENV['RAILS_AIRBRAKE_PORT'] || 443).to_i
+  config.secure      = config.port == 443
+  config.ignore         << ActionController::MethodNotAllowed
+  config.ignore         << ActionController::RoutingError
+  config.ignore         << ActionController::UnknownHttpMethod
+  config.params_filters << 'RAILS_DB_PASSWORD'
+  config.params_filters << 'RAILS_AIRBRAKE_API_KEY'
+  config.params_filters << 'RAILS_SECRET_TOKEN'
+end
