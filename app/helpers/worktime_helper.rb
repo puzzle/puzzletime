d35d8ff4c224d11c57ef593902a3c0ae96a1454b
Bruno Santschi <santschi@puzzle.ch>
2014-06-17 16:45:52 +0200

    Colorize times in day overview

--- app/helpers/worktime_helper.rb
+++ app/helpers/worktime_helper.rb
@@ -33,6 +33,14 @@ module WorktimeHelper
     description   
   end
   
+  def overview_day_class(worktimes, day)
+    if Holiday.holiday?(day)
+      'holiday'
+    elsif day < Date.today && sum_daily_worktimes(worktimes, day) <= 0
+      'missing'
+    end
+  end
+  
   def time_range(worktime)
     result = ""
     if worktime.from_start_time.present?
