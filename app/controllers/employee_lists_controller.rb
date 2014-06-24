cc0d3d887817c7e57915d5648ba6ca1388e0ade0
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-06-24 10:31:43 +0200

    refactor employee lists to crud controller

--- app/controllers/employee_lists_controller.rb
+++ app/controllers/employee_lists_controller.rb
@@ -1,98 +1,35 @@
 # encoding: utf-8
 
-# TODO: inherit from crud controller
-class EmployeeListsController < ApplicationController
+class EmployeeListsController < CrudController
 
   before_action :set_period
 
+  before_render_show :set_employees
+  before_render_form :set_current_employees
 
-  # GET /employee_lists/1
-  # GET /employee_lists/1.xml
-  def show
-    @employee_list = EmployeeList.find(params[:id])
-    @employees = @employee_list.employees.order(:lastname)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render xml: @employee_list }
-    end
-  end
-
-  # GET /employee_lists/new
-  # GET /employee_lists/new.xml
-  def new
-    @employee_list = EmployeeList.new
-    @curr_employees = Employee.employed_ones(@period || Period.past_month)
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render xml: @employee_list }
-    end
-  end
-
-  # GET /employee_lists/1/edit
-  def edit
-    @employee_list = EmployeeList.find(params[:id])
-    @curr_employees = Employee.employed_ones(@period || Period.past_month)
-  end
-
-  # POST /employee_lists
-  # POST /employee_lists.xml
-  def create
-    @employee_list = EmployeeList.new(model_params)
-    @employee_list.employee_id = @user.id # add current user id to the created object
-
-    # logger.debug "employees = #{@employee_list.employees.inspect}"
-    respond_to do |format|
-      if @employee_list.save
-        flash[:notice] = 'Mitarbeiterliste wurde erfolgreich erstellt.'
-        # format.html { redirect_to(@employee_list) }
-        format.html { redirect_to controller: 'plannings', action: 'employee_lists' }
-        format.xml  { render xml: @employee_list, status: :created, location: @employee_list }
-      else
-        format.html do
-          @curr_employees = Employee.employed_ones(@period || Period.past_month)
-          render action: 'new'
-        end
-        format.xml  { render xml: @employee_list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /employee_lists/1
-  # PUT /employee_lists/1.xml
-  def update
-    params[:employee_list][:employee_ids] ||= []
-
-    @employee_list = EmployeeList.find(params[:id])
-
-    respond_to do |format|
-      if @employee_list.update_attributes(model_params)
-        flash[:notice] = 'Mitarbeiterliste wurde erfolgreich angepasst.'
-        # format.html { redirect_to(@employee_list) }
-        format.html { redirect_to controller: 'plannings', action: 'employee_lists' }
-        format.xml  { head :ok }
-      else
-        format.html { render action: 'edit' }
-        format.xml  { render xml: @employee_list.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /employee_lists/1
-  # DELETE /employee_lists/1.xml
-  def destroy
-    @employee_list = EmployeeList.find(params[:id])
-    @employee_list.destroy
-
-    respond_to do |format|
-      # format.html { redirect_to(employee_lists_url) }
-      format.html { redirect_to controller: 'plannings', action: 'employee_lists' }
-      format.xml  { head :ok }
-    end
+  def index
+    @employee = @user
+    super
   end
 
   private
+
+  def list_entries
+    super.where(employee_id: @user.id)
+  end
+
+  def set_employees
+    @employees = @employee_list.employees.order(:lastname)
+  end
+
+  def set_current_employees
+    @curr_employees = Employee.employed_ones(@period || Period.past_month)
+  end
+
+  def assign_attributes
+    super
+    entry.employee_id = @user.id
+  end
 
   def model_params
     params.require(:employee_list).permit(:title, :employee_ids => [])
