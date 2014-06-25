cac06fea3ccc3659f005d024fb1f98f35fe2d784
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-06-25 13:32:21 +0200

    patch erd to work with abstract classes

---
+++ lib/tasks/erd_patch.rb
@@ -0,0 +1,19 @@
+require 'rails_erd/domain/specialization'
+
+module RailsERD
+  class Domain
+    class Specialization
+      class << self
+        def abstract_from_models(domain, models)
+          models.select(&:abstract_class?).
+                 collect(&:descendants).
+                 flatten.
+                 select  { |model| domain.entity_by_name(model.name) }.
+                 collect { |model|
+            new(domain, domain.entity_by_name(model.superclass.name), domain.entity_by_name(model.name))
+          }
+        end
+      end
+    end
+  end
+end
