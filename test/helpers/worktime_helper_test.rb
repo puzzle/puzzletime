f4758023bbd15027139293c7b3020ae30f9cd4b7
Bruno Santschi <santschi@puzzle.ch>
2014-07-15 13:47:35 +0200

    Fix helper tests

--- test/helpers/worktime_helper_test.rb
+++ test/helpers/worktime_helper_test.rb
@@ -8,18 +8,19 @@ class WorktimeHelperTest < ActionView::TestCase
 
   def setup
     @worktimes = Worktime.where('employee_id = ? AND work_date >= ? AND work_date <= ?', 7, Date.new(2006, 12, 4), Date.new(2006, 12, 10))
+    @daily_worktimes = @worktimes.group_by{ |w| w.work_date}
   end
 
   test 'daily worktimes' do
-    assert_equal [worktimes(:wt_mw_webauftritt)], daily_worktimes(@worktimes, Date.new(2006, 12, 8))
+    assert_equal [worktimes(:wt_mw_webauftritt)], @daily_worktimes[Date.new(2006, 12, 8)]
   end
 
   test 'sum daily worktimes' do
-    assert_equal 8, sum_daily_worktimes(@worktimes, Date.new(2006, 12, 9))
+    assert_equal 8, sum_hours(Date.new(2006, 12, 9))
   end
 
   test 'sum total worktimes' do
-    assert_equal 26, sum_total_worktimes(@worktimes)
+    assert_equal 26, sum_hours
   end
 
   test 'worktime account' do
