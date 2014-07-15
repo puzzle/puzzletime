72b72e628ec094dba8e1071d389397e662e57c53
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-07-15 14:38:50 +0200

    add target scopes controller

---
+++ test/controllers/target_scopes_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class TargetScopesControllerTest < ActionController::TestCase
+
+  include CrudControllerTestHelper
+
+  setup :login
+
+  not_existing :test_show,
+               :test_show_json,
+               :test_show_with_non_existing_id_raises_record_not_found
+
+  private
+
+  # Test object used in several tests.
+  def test_entry
+    target_scopes(:time)
+  end
+
+  # Attribute hash used in several tests.
+  def test_entry_attrs
+    { name: 'Happiness',
+      position: 5,
+      icon: 'user' }
+  end
+end
--- test/fabricators/employee_fabricator.rb
+++ test/fabricators/employee_fabricator.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                    :integer          not null, primary key
+#  firstname             :string(255)      not null
+#  lastname              :string(255)      not null
+#  shortname             :string(3)        not null
+#  passwd                :string(255)
+#  email                 :string(255)      not null
+#  management            :boolean          default(FALSE)
+#  initial_vacation_days :float
+#  ldapname              :string(255)
+#  eval_periods          :string(3)        is an Array
+#  department_id         :integer
+#
+
 
 Fabricator(:employee) do
   firstname { Faker::Name.first_name }
