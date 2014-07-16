c2847ef2540bf44c482c6264a57a650ac8d5a3f6
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-07-16 17:58:41 +0200

    start with order form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -9,4 +9,9 @@ class ClientsController < ManageController
 
   self.permitted_attrs = :name, :shortname
 
+  def new
+    super do |format|
+      format.js { render partial: 'form' }
+    end
+  end
 end
