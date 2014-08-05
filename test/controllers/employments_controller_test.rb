093c609834a417d294b552907529a6e6daff7011
Bruno Santschi <santschi@puzzle.ch>
2014-08-05 10:04:42 +0200

    Test overlapping employments

--- test/controllers/employments_controller_test.rb
+++ test/controllers/employments_controller_test.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'test_helper'
 
 class EmploymentsControllerTest < ActionController::TestCase
@@ -9,6 +10,15 @@ class EmploymentsControllerTest < ActionController::TestCase
   not_existing :test_show,
                :test_show_json,
                :test_show_with_non_existing_id_raises_record_not_found
+
+  def test_overlapping
+    assert_equal Date.new(2006, 12, 31), employments(:for_half_year).end_date
+    post :create, employment: { percent: 100,
+                               start_date: Date.new(2006, 10, 1),
+                               end_date: Date.new(2007, 5, 31)}, employee_id: 1
+    assert response.body.include? "Für diese Zeitspanne ist bereits eine andere Anstellung definiert"
+  end
+
 
   private
 
