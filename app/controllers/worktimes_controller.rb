9f4f219e7b1f371ec0a4633c3433d41444ff3f43
Bruno Santschi <santschi@puzzle.ch>
2014-08-07 13:28:16 +0200

    Fix n+1 query

--- app/controllers/worktimes_controller.rb
+++ app/controllers/worktimes_controller.rb
@@ -154,7 +154,7 @@ class WorktimesController < CrudController
 
   def list_entries
     @worktimes = Worktime.where('employee_id = ? AND work_date >= ? AND work_date <= ?', @user.id, @week_days.first, @week_days.last)
-                         .includes(:work_item)
+                         .includes(:work_item, :absence)
                          .order('work_date, from_start_time, work_item_id')
     @daily_worktimes = @worktimes.group_by{ |w| w.work_date }
     @worktimes
