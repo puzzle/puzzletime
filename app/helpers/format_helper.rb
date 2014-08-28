9a0a00d427a279e77c7b0c37874bfa5f9798d966
Bruno Santschi <santschi@puzzle.ch>
2014-08-28 12:53:13 +0200

    Rollback to old (industrial) hour format

--- app/helpers/format_helper.rb
+++ app/helpers/format_helper.rb
@@ -35,12 +35,15 @@ module FormatHelper
     format_type(obj, attr)
   end
 
+  # round time function.
   def format_hour(hour)
-    if hour
-      minutes = ((hour - hour.floor) * 60).round.to_s.rjust(2, "0")
-      hours = number_with_delimiter(hour.floor, :delimiter => "'")
-      "#{hours}:#{minutes}".html_safe
-    end
+    return unless hour
+    # number_with_precision is not that performant
+    number = (Float(hour) * (100)).round.to_f / 100
+    number = '%01.2f' % number
+    parts = number.split('.')
+    parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1'")
+    parts.join('.')
   end
 
   def format_time(time)
