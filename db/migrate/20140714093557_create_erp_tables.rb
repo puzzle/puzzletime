69983cc2f1df79696b7fec3b0f735a5b978c3e41
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-10-27 15:53:26 +0100

    add offered_total to accounting posts

--- db/migrate/20140714093557_create_erp_tables.rb
+++ db/migrate/20140714093557_create_erp_tables.rb
@@ -171,6 +171,7 @@ class CreateErpTables < ActiveRecord::Migration
       t.string :reference
       t.integer :offered_hours
       t.integer :offered_rate
+      t.decimal :offered_total, precision: 12, scale: 2
       t.integer :discount_percent
       t.integer :discount_fixed
       t.string :report_type
