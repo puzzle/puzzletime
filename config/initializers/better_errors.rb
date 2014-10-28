a046bf92fb68f909c9b4e73f3a6b14897d517a1c
Daniel Illi-Zuberbühler <illi@puzzle.ch>
2014-10-28 15:42:53 +0100

    fix better errors initializer

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -1,4 +1,4 @@
-if defined? BetterErrors && ENV['BETTER_ERRORS_URL'].present?
+if defined?(BetterErrors) && ENV['BETTER_ERRORS_URL'].present?
   BetterErrors.editor = proc { |full_path, line|
     namespace = OpenStruct.new(full_path: "/hello/world", line: 123)
     Haml::Engine.new(ENV['BETTER_ERRORS_URL']).render(namespace.instance_eval { binding})
