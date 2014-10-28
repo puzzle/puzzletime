94147699f560d7c75f7653aa6ea7a00efb7a2399
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-10-28 11:40:45 +0100

    add cockpit overview values, refs #7115

--- app/domain/order/cockpit/accounting_post_row.rb
+++ app/domain/order/cockpit/accounting_post_row.rb
@@ -17,51 +17,64 @@ class Order::Cockpit
       accounting_post.offered_rate
     end
 
+    def supplied_services_hours
+      accounting_post_hours.values.sum.round
+    end
+
+    def not_billable_hours
+      (accounting_post_hours[false] || 0).round
+    end
+
     private
 
     def build_cells
-      [ build_budget_cell,
-        build_productivity_cell,
-        build_billed_cell,
-        build_open_productivity_cell,
-        build_not_billable_cell,
-        build_remaining_cell,
-        build_open_budget_cell ]
+      { budget:            build_budget_cell,
+        supplied_services: build_supplied_services_cell,
+        billed:            build_billed_cell,
+        open_services:     build_open_services_cell,
+        not_billable:      build_not_billable_cell,
+        remaining:         build_remaining_cell,
+        open_budget:       build_open_budget_cell }
     end
 
     def build_budget_cell
       Cell.new(accounting_post.offered_hours, accounting_post.offered_total)
     end
 
-    def build_productivity_cell
-      hours = accounting_post_hours.values.sum
-      amount = offered_rate && offered_rate * hours
-      Cell.new(hours, amount)
+    def build_supplied_services_cell
+      build_cell_with_amount(supplied_services_hours)
     end
 
     # TODO
     def build_billed_cell
-      Cell.new(nil, nil)
+      # (Ist-R[h])
+      Cell.new(0, 0)
     end
 
     # TODO
-    def build_open_productivity_cell
-      Cell.new(nil, nil)
+    def build_open_services_cell
+      # (Ist[h] - Ist-R[h])*Soll[CHF/h]
+      Cell.new(0, 0)
     end
 
     def build_not_billable_cell
-      hours = accounting_post_hours[false]
-      Cell.new(hours, nil)
+      Cell.new(not_billable_hours, nil)
     end
 
-    # TODO
     def build_remaining_cell
-      Cell.new(nil, nil)
+      # Soll[h] - Ist-R[h]
+      # Soll[CHF] - Ist-R[CHF]
+      Cell.new(accounting_post.remaining_hours || 0, nil)
     end
 
     # TODO
     def build_open_budget_cell
-      Cell.new(nil, nil)
+      Cell.new(0, 0)
+    end
+
+    def build_cell_with_amount(hours)
+      amount = offered_rate && offered_rate * hours
+      Cell.new(hours, amount)
     end
 
     def accounting_post_hours
---
+++ test/models/domain/order/cockpit_test.rb
@@ -0,0 +1,149 @@
+require 'test_helper'
+
+class Order::CockpitTest < ActiveSupport::TestCase
+
+  test 'cockpit has row for each accounting post and total' do
+    assert_equal 3, cockpit.rows.size
+    assert_kind_of Order::Cockpit::TotalRow, cockpit.rows.first
+    assert_equal accounting_posts(:hitobito_demo_app), cockpit.rows.second.accounting_post
+    assert_equal accounting_posts(:hitobito_demo_site), cockpit.rows.third.accounting_post
+  end
+
+  test 'budget values are nil if nothing is offered' do
+    total = cockpit.rows.first
+    budget = total.cells[:budget]
+    assert_equal nil, budget.hours
+    assert_equal nil, budget.days
+    assert_equal nil, budget.amount
+
+    a1 = cockpit.rows.second
+    budget = a1.cells[:budget]
+    assert_equal nil, budget.hours
+    assert_equal nil, budget.days
+    assert_equal nil, budget.amount
+  end
+
+  test 'budget values are based on offered fields' do
+    define_offered_fields
+
+    total = cockpit.rows.first
+    budget = total.cells[:budget]
+    assert_equal 300, budget.hours
+    assert_equal 37.5, budget.days
+    assert_equal 39_000.0, budget.amount
+
+    a1 = cockpit.rows.second
+    budget = a1.cells[:budget]
+    assert_equal 200, budget.hours
+    assert_equal 25.0, budget.days
+    assert_equal 24_000, budget.amount
+  end
+
+  test 'supplied_services values are zero if nothing is offered' do
+    total = cockpit.rows.first
+    supplied = total.cells[:supplied_services]
+    assert_equal 0, supplied.hours
+    assert_equal 0, supplied.days
+    assert_equal nil, supplied.amount
+
+    a1 = cockpit.rows.second
+    supplied = a1.cells[:supplied_services]
+    assert_equal 0, supplied.hours
+    assert_equal 0, supplied.days
+    assert_equal nil, supplied.amount
+  end
+
+  test 'productivity values are zero if no worktimes exist' do
+    define_offered_fields
+
+    total = cockpit.rows.first
+    supplied = total.cells[:supplied_services]
+    assert_equal 0, supplied.hours
+    assert_equal 0, supplied.days
+    assert_equal 0, supplied.amount
+
+    a1 = cockpit.rows.second
+    supplied = a1.cells[:supplied_services]
+    assert_equal 0, supplied.hours
+    assert_equal 0, supplied.days
+    assert_equal 0, supplied.amount
+  end
+
+  test 'productivity values are calculated if worktimes exist' do
+    define_offered_fields
+    define_worktimes
+
+    total = cockpit.rows.first
+    supplied = total.cells[:supplied_services]
+    assert_equal 26, supplied.hours
+    assert_equal 3.25, supplied.days
+    assert_equal 3420.0, supplied.amount
+
+    a1 = cockpit.rows.second
+    supplied = a1.cells[:supplied_services]
+    assert_equal 16, supplied.hours
+    assert_equal 2.0, supplied.days
+    assert_equal 1920, supplied.amount
+  end
+
+  test 'not billable values are calculated if worktimes exist' do
+    define_offered_fields
+    define_worktimes
+
+    total = cockpit.rows.first
+    not_billable = total.cells[:not_billable]
+    assert_equal 4, not_billable.hours
+    assert_equal 0.5, not_billable.days
+    assert_equal nil, not_billable.amount
+
+    a1 = cockpit.rows.second
+    not_billable = a1.cells[:not_billable]
+    assert_equal 4, not_billable.hours
+    assert_equal 0.5, not_billable.days
+    assert_equal nil, not_billable.amount
+
+    a2 = cockpit.rows.third
+    not_billable = a2.cells[:not_billable]
+    assert_equal 0, not_billable.hours
+    assert_equal 0, not_billable.days
+    assert_equal nil, not_billable.amount
+  end
+
+  def define_offered_fields
+    accounting_posts(:hitobito_demo_app).update!(offered_hours: 200, offered_rate: 120)
+    accounting_posts(:hitobito_demo_site).update!(offered_hours: 100, offered_rate: 150)
+  end
+
+  def define_worktimes
+    Ordertime.create!(work_item: work_items(:hitobito_demo_app),
+                      employee: employees(:pascal),
+                      work_date: Date.today,
+                      report_type: HoursDayType::INSTANCE,
+                      hours: 8)
+    Ordertime.create!(work_item: work_items(:hitobito_demo_app),
+                      employee: employees(:pascal),
+                      work_date: Date.today - 1,
+                      report_type: HoursDayType::INSTANCE,
+                      hours: 4)
+    Ordertime.create!(work_item: work_items(:hitobito_demo_app),
+                      employee: employees(:pascal),
+                      work_date: Date.today - 1,
+                      report_type: HoursDayType::INSTANCE,
+                      billable: false,
+                      hours: 4)
+    Ordertime.create!(work_item: work_items(:hitobito_demo_site),
+                      employee: employees(:pascal),
+                      work_date: Date.today - 2,
+                      report_type: HoursDayType::INSTANCE,
+                      hours: 10)
+  end
+
+  def order
+    @order ||= orders(:hitobito_demo)
+  end
+
+  def cockpit
+    @cockpit ||= Order::Cockpit.new(order)
+  end
+
+end
