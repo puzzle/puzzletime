571708182f80ac9324ec008e4f0e7fd044633d67
Daniel Illi <illi@puzzle.ch>
2014-12-09 09:36:03 +0100

    make migration reversible

--- db/migrate/20141208095937_create_working_conditions.rb
+++ db/migrate/20141208095937_create_working_conditions.rb
@@ -6,6 +6,8 @@ class CreateWorkingConditions < ActiveRecord::Migration
       t.decimal :must_hours_per_day, precision: 4, scale: 2, null: false
     end
 
-    WorkingCondition.create!(vacation_days_per_year: 25, must_hours_per_day: 8)
+    reversible do |dir|
+      dir.up { WorkingCondition.create!(vacation_days_per_year: 25, must_hours_per_day: 8) }
+    end
   end
 end
