3015d544c4894995b9b24b4ae38d68a3cd684759
Daniel Illi <illi@puzzle.ch>
2015-04-27 13:59:05 +0200

    fix db schema.rb and migration

--- db/migrate/20150325153624_add_from_to_times_required_to_accounting_post.rb
+++ db/migrate/20150325153624_add_from_to_times_required_to_accounting_post.rb
@@ -1,5 +1,5 @@
 class AddFromToTimesRequiredToAccountingPost < ActiveRecord::Migration
-  def up
+  def change
     # added column retroactively to migration '20140714093557_create_erp_tables'
     # we keep this migration for environments where the original create_erp_tables migration was already run
     unless column_exists? :accounting_posts, :from_to_times_required
