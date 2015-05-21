9637ef001892bac47eba9052f6977fcd5b8113ca
Daniel Illi <illi@puzzle.ch>
2015-05-21 15:58:07 +0200

    order_services: fix for worktime edit link, now works even when order.work_item != accounting_post.work_itemallow order responsible to edit worktimes. fixes #10637

--- test/integration/show_order_services_test.rb
+++ test/integration/show_order_services_test.rb
@@ -1,37 +1,105 @@
 require 'test_helper'
 
 class ShowOrderServices < ActionDispatch::IntegrationTest
+
   test 'click on worktime row as employee does not open edit view' do
     timeout_safe do
-      show_order_services_as :pascal
-      all("#ordertime_6 td")[2].click
+      create_ordertime_show_order_services_as employee_without_responsibilities
+      click_worktime_row
       assert has_no_text?("Zeit bearbeiten")
-      assert_equal order_order_services_path(order_id: orders(:puzzletime)), current_path
+      assert_equal order_order_services_path(order_id: order), current_path
     end
   end
 
-  test 'click on worktime row as employee opens edit view' do
+  test 'click on worktime row as order responsible opens edit view' do
     timeout_safe do
-      show_order_services_as :lucien
-      all("#ordertime_6 td")[2].click
+      create_ordertime_show_order_services_as employee_responsible_for_order
+      click_worktime_row
       assert has_text?("Zeit bearbeiten")
-      assert_equal edit_ordertime_path(id: 6), current_path
+      assert_equal edit_ordertime_path(id: ordertime.id), current_path
+    end
+  end
+
+  test 'click on worktime row as order responsible for different order does not open edit view' do
+    timeout_safe do
+      create_ordertime_show_order_services_as employee_responsible_for_different_order
+      click_worktime_row
+      assert has_no_text?("Zeit bearbeiten")
+      assert_equal order_order_services_path(order_id: order), current_path
     end
   end
 
   test 'click on worktime row as management opens edit view' do
     timeout_safe do
-      show_order_services_as :mark
-      all("#ordertime_10 td")[2].click
+      create_ordertime_show_order_services_as manager_not_responsible_for_any_order
+      click_worktime_row
       assert has_text?("Zeit bearbeiten")
-      assert_equal edit_ordertime_path(id: 10), current_path
+      assert_equal edit_ordertime_path(id: ordertime.id), current_path
     end
   end
 
   private
 
-  def show_order_services_as(employee)
+  def employee_without_responsibilities
+    Employee.where.not(management: true, id: responsible_ids).first.tap do |employee|
+      refute employee.management
+      refute employee.order_responsible?
+    end
+  end
+
+  def employee_responsible_for_order
+    order.responsible.tap do |employee|
+      refute employee.management
+      assert employee.order_responsible?
+      assert_equal employee, order.responsible
+    end
+  end
+
+  def employee_responsible_for_different_order
+    Employee.where(management: false, id: responsible_ids).where.not(id: order.responsible_id).first.tap do |employee|
+      refute employee.management
+      assert employee.order_responsible?
+      refute_equal employee, order.responsible
+    end
+  end
+
+  def manager_not_responsible_for_any_order
+    Employee.where(management: true).where.not(id: order.responsible_id).first.tap do |employee|
+      refute_equal employee, order.responsible
+      assert employee.management
+    end
+  end
+
+  def responsible_ids
+    Order.select(:responsible_id).uniq.pluck(:responsible_id)
+  end
+
+  def create_ordertime(employee)
+    @ordertime = Ordertime.create!(
+        employee: employee,
+        work_date: Date.today,
+        report_type: :absolute_day,
+        hours: 0.5,
+        description: "some doodling",
+        work_item: work_items(:hitobito_demo_app)
+    )
+  end
+
+  def ordertime
+    @ordertime
+  end
+
+  def order
+    orders(:hitobito_demo)
+  end
+
+  def click_worktime_row
+    all("#ordertime_#{ordertime.id} td")[2].click
+  end
+
+  def create_ordertime_show_order_services_as(employee)
+    create_ordertime employee
     login_as employee
-    visit order_order_services_path(order_id: orders(:puzzletime))
+    visit order_order_services_path(order_id: order)
   end
 end
