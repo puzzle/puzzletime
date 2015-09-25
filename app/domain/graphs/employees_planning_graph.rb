f4539db327d1f17bab437c6cf127b5fd1081b7ce
Daniel Illi-Zuberbühler <illi@puzzle.ch>
2015-09-25 17:41:09 +0200

    company planning: sort employees according to their planned load ascending. fixes #11067

--- app/domain/graphs/employees_planning_graph.rb
+++ app/domain/graphs/employees_planning_graph.rb
@@ -7,14 +7,15 @@ class EmployeesPlanningGraph
   attr_reader :period
   attr_reader :employee_graphs
 
-  def initialize(employees, period = nil)
+  def initialize(employees, period = nil, sort_by_load = false)
     @employees = employees
     period ||= Period.next_three_months
     @actual_period = period
     @period = period.extend_to_weeks
     @colorMap = AccountColorMapper.new
 
-    @employee_graphs = @employees.map { |employee| EmployeePlanningGraph.new(employee, period) }.sort_by {|graph| graph.period_load.to_f }
+    sort_attr = sort_by_load ? :period_load : :employee
+    @employee_graphs = @employees.map { |employee| EmployeePlanningGraph.new(employee, period) }.sort_by(&sort_attr)
   end
 
   def color_for(work_item)
