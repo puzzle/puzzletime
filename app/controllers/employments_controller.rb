107758e17b8b52ab5014ef2689fc3e2d5771af47
Mathis Hofer <hofer@puzzle.ch>
2016-07-07 13:56:16 +0200

    employment: add commen and propation end fields, refs #15796, #15260

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -3,5 +3,6 @@
 class EmploymentsController < ManageController
   self.nesting = Employee
 
-  self.permitted_attrs = :percent, :start_date, :end_date, :vacation_days_per_year
+  self.permitted_attrs = :percent, :start_date, :end_date, :probation_period_end_date,
+    :vacation_days_per_year, :comment
 end
