377af9995d68a4dc4175cda19c1ca8c8c3d8e0d9
Damian Senn <senn@puzzle.ch>
2016-09-12 16:39:06 +0200

    Set main contact in small_invoice sync. Fixes #20498

--- app/domain/invoicing/small_invoice/entity/client.rb
+++ app/domain/invoicing/small_invoice/entity/client.rb
@@ -10,7 +10,15 @@ module Invoicing
             language: constant(:language),
             einvoice_account_id: entry.e_bill_account_key,
             addresses: entry.billing_addresses.collect { |a| Address.new(a).to_hash },
-            contacts: entry.contacts.collect { |c| Entity::Contact.new(c).to_hash }
+            contacts: entry.contacts.collect.with_index do |c, i|
+              # Set primary on the first contact to ensure we always have a
+              # primary contact for this client.
+              # Clients can only have one primary contact,
+              # setting more than one contact to primary will overwrite the
+              # existing one.
+              # See #20498
+              Entity::Contact.new(c).to_hash.update(primary: i == 0)
+            end
           }
         end
       end
