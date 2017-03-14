89a3fe5d1ff4463220896713b3d82e030ff14294
Damian Senn <senn@puzzle.ch>
2017-03-14 11:51:30 +0100

    Add "Speichern & Neu" button to ordertime form
    Refs #23795

--- app/controllers/worktimes_controller.rb
+++ app/controllers/worktimes_controller.rb
@@ -39,6 +39,15 @@ class WorktimesController < CrudController
     end
   end
 
+  def create(options = {})
+    if params[:redirect_to_self]
+      work_date = params[:ordertime][:work_date]
+      options[:location] = new_ordertime_path(work_date: work_date)
+    end
+
+    super(options)
+  end
+
   # ajax action
   def existing
     @worktime = Worktime.new
