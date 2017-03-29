464ba0156e691641700390711d1a0beddd54db5f
Damian Senn <senn@puzzle.ch>
2017-03-29 14:05:19 +0200

    Remove dead code

--- test/models/employment_roles_employment_test.rb
+++ test/models/employment_roles_employment_test.rb
@@ -75,23 +75,5 @@ class EmploymentRolesEmploymentTest < ActiveSupport::TestCase
     end
 
     assert_match(/Pensum ist kein gültiger Wert/, err.message)
-
-    # assert_nothing_raised do
-    #   EmploymentRolesEmployment.create!(employment: employments(:burnout),
-    #                                     employment_role: employment_roles(:technical_board),
-    #                                     percent: 0)
-    #   EmploymentRolesEmployment.create!(employment: employments(:burnout),
-    #                                     employment_role: employment_roles(:software_engineer),
-    #                                     employment_role_level: employment_role_levels(:senior),
-    #                                     percent: 100)
-    #   EmploymentRolesEmployment.create!(employment: employments(:burnout),
-    #                                     employment_role: employment_roles(:system_engineer),
-    #                                     employment_role_level: employment_role_levels(:senior),
-    #                                     percent: 50)
-    #   EmploymentRolesEmployment.create!(employment: employments(:burnout),
-    #                                     employment_role: employment_roles(:project_manager),
-    #                                     employment_role_level: employment_role_levels(:professional),
-    #                                     percent: 50)
-    # end
   end
 end
