091b5de590dcf2cbbfc1d892af6fc0833e5482fa
Mathis Hofer <hofer@puzzle.ch>
2017-06-15 09:32:49 +0200

    enable total details for absences evaluation, refs #24806

--- app/domain/evaluations/absences_eval.rb
+++ app/domain/evaluations/absences_eval.rb
@@ -5,7 +5,6 @@ class AbsencesEval < Evaluation
   self.division_column  = :employee_id
   self.label            = 'Mitarbeiter Absenzen'
   self.absences         = true
-  self.total_details    = false
   self.detail_columns   = detail_columns.reject { |i| :billable == i }
   self.detail_labels    = detail_labels.merge(account: 'Absenz')
 
