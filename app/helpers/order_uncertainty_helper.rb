deac0684cd5126fba611d37ea5f9c38b671e7646
Mathis Hofer <hofer@puzzle.ch>
2017-07-19 11:14:18 +0200

    order: risk/chance icon improvements, refs #24984

--- app/helpers/order_uncertainty_helper.rb
+++ app/helpers/order_uncertainty_helper.rb
@@ -1,20 +1,45 @@
 module OrderUncertaintyHelper
   def format_probability(value)
-    t("activerecord.attributes.order_uncertainty.probabilities.#{value.probability}")
+    t("activerecord.attributes.order_uncertainty/probabilities.#{value.probability}")
   end
 
   def format_impact(value)
-    t("activerecord.attributes.order_uncertainty.impacts.#{value.impact}")
+    t("activerecord.attributes.order_uncertainty/impacts.#{value.impact}")
   end
 
   def format_risk(value)
-    risk_icon = value.is_a?(OrderRisk) ? 'cloud' : 'clover'
-    risk_class = "order-#{value.is_a?(OrderRisk) ? 'risk' : 'chance'}-icon #{value.risk}"
-    picon(risk_icon, title: t("activerecord.attributes.order_uncertainty.risks.#{value.risk}"),
-                     class: risk_class)
+    content_tag(:span, safe_join(risk_icons(value.risk, value.type)),
+                title: t("activerecord.attributes.order_uncertainty/risks.#{value.risk}"))
   end
 
   def format_measure(value)
     auto_link(value.measure)
+  end
+
+  private
+
+  def risk_icons(risk, type)
+    count = risk_icon_count(risk)
+    return [] if count.zero?
+
+    icon_name = type == OrderRisk.sti_name ? 'cloud' : 'clover'
+    (1..3).map do |i|
+      icon_class = "order-#{type == OrderRisk.sti_name ? 'risk' : 'chance'}-icon"
+      icon_class += ' inactive' if i > count
+      picon(icon_name, class: icon_class)
+    end
+  end
+
+  def risk_icon_count(risk)
+    case risk
+    when :high
+      3
+    when :medium
+      2
+    when :low
+      1
+    else
+      0
+    end
   end
 end
