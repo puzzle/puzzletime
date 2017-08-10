8c7ef0989fe7f862f9bc09a513537fafd94c0f2b
Severin Räz <raez@puzzle.ch>
2017-08-10 17:39:47 +0200

    Remove timezone, fix time on OpenShift

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,10 @@ module Puzzletime
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Bern'
+    # Attention: Setting a time zone here will confuse OpenShift.
+    # We leave the Time zone on UTC as recommended by
+    # https://robots.thoughtbot.com/its-about-time-zones
+    # config.time_zone = 'Bern'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
