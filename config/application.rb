ce886eb9d5712b002fa0e157ea32e482aecc4cf3
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-01-21 14:59:58 +0100

    Bump rails version to 5.2.2 and update configuration files

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,12 @@ require_relative 'version'
 module Puzzletime
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    #config.load_defaults 5.1
+    #config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     config.autoload_paths += %W(#{config.root}/app/domain/forms
                                 #{config.root}/app/domain/reports
