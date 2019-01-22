1b47efcbd2f8e1a6610c458d562a881d47848cef
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-01-23 00:37:23 +0100

    Fix failing test data creation
    Rails now enforces belongs_to associations by default. This does not work well for our test data generation method. And since the relation is written into the in-code migration, we turn it off globally.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Puzzletime
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.active_record.belongs_to_required_by_default = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
