941968913d3ed749c22d84a1541401db4054258c
Andreas Maierhofer <a.maierhofer@gmail.com>
2019-01-28 14:16:03 +0100

    Refuse to start if we cannot connect to memcached

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,6 @@
 
 #Rails.application.config.session_store :cookie_store, key: '_puzzletime_session'
 Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 12.hours
-
+unless Rails.env.test? || Rails.cache.dalli.stats.values.any?(&:present?)
+  fail "As CSRF tokens are read from cache, we require a memcache instance to start"
+end
