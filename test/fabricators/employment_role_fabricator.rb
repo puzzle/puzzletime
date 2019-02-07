69817705d79fbf8306e225271351010851701ac5
Daniel Illi <illi@puzzle.ch>
2019-02-07 17:09:23 +0100

    Make fabricators/employment_role_fabricator.rb more reliable

--- test/fabricators/employment_role_fabricator.rb
+++ test/fabricators/employment_role_fabricator.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Fabricator(:employment_role) do
-  name     { "#{Faker::Cat.breed} #{Time.now.to_i}" }
+  name     { sequence(:employment_role) { |i| "employment-role-#{i}" } }
   billable { false }
   level    { false }
 end
