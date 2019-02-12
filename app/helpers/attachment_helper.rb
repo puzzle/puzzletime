b6ea4010437cad074fef68bd74e71aba9b709124
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-02-12 17:06:49 +0100

    Modify expenses show page to show the attachment preview

---
+++ app/helpers/attachment_helper.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module AttachmentHelper
+  def attachment_download_link(obj, **options)
+    link_to(obj.filename, rails_blob_path(obj, disposition: 'attachment'), options)
+  end
+
+  def attachment_show_link(obj, label, **options)
+    link_to(label, rails_blob_path(obj), options)
+  end
+
+  def attachment_image_tag(obj, **options)
+    path =
+      if obj.image?
+        rails_blob_url(obj)
+      elsif obj.previewable?
+        obj.preview({})
+      end
+    image_tag path, options
+  end
+
+end
