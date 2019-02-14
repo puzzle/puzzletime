5491782f6f5a3d2f9da6f3318d3eac91a06053dd
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-02-14 16:24:58 +0100

    Fix bug with rails console on OpenShift

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -16,6 +16,7 @@ end
 
 Rails.application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 12.hours
 
-if memcache_configured? && !dalli_reachable?
+# We expect memcache to work in production. Prevents an error with the rails console on OpenShift
+if memcache_configured? && !Rails.env.production? && !dalli_reachable?
   fail "As CSRF tokens are read from cache, we require a memcache instance to start"
 end
