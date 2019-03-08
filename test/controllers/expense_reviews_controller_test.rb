e80ba47a9ad8aa86003f6a06dd7556d40a672685
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-03-08 16:21:48 +0100

    Fix tests and fixtures

--- test/controllers/expense_reviews_controller_test.rb
+++ test/controllers/expense_reviews_controller_test.rb
@@ -41,7 +41,7 @@ class ExpenseReviewsControllerTest < ActionController::TestCase
 
   test 'POST#create approves expenses and redirects to next open expense if any' do
     login_as(:mark)
-    other = Expense.create!(employee: employees(:pascal), payment_date: '2019-02-02', kind: :training, amount: 1)
+    other = Expense.create!(employee: employees(:pascal), payment_date: '2019-02-02', kind: :training, description: 'test',  amount: 1)
     get :create, params: { expense_id: expenses(:pending).id, expense: { status: :approved, reimbursement_date: '2019-03-01' } }
     assert_redirected_to expense_review_path(other)
   end
@@ -49,7 +49,7 @@ class ExpenseReviewsControllerTest < ActionController::TestCase
   test 'POST#create approves expenses and redirects to next open expense if any of status set on list view' do
     login_as(:mark)
     list_params = { '/expenses' => { 'status' => Expense.statuses['deferred'] } }
-    other = Expense.create!(employee: employees(:pascal), status: :deferred, payment_date: '2019-02-02', kind: :training, amount: 1)
+    other = Expense.create!(employee: employees(:pascal), status: :deferred, payment_date: '2019-02-11', kind: :training, description: 'test', amount: 1)
     get :create, params: { expense_id: expenses(:pending).id, expense: { status: :approved, reimbursement_date: '2019-03-01' } }, session: { 'list_params' => list_params }
     assert_redirected_to expense_review_path(other)
   end
