7abc35dc34b858a6e7f56c15f3f4d2b35c894ddc
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-04-29 16:15:23 +0200

    Implement CSV Export

--- app/domain/reports/revenue/csv.rb
+++ app/domain/reports/revenue/csv.rb
@@ -18,7 +18,7 @@ class Reports::Revenue::Csv
 
       entries.each do |e|
         @entry = e
-        csv << row(entry)
+        csv << row
       end
 
       csv << footer
@@ -27,16 +27,20 @@ class Reports::Revenue::Csv
 
   private
 
+  # Headers
+
   def header
-    [header_grouping, header_past_months, header_past_months_summary, header_future_months]
+    [header_grouping, header_past_months, header_past_months_summary, header_future_months].flatten
   end
 
   def header_grouping
-    @report.grouping_name_human
+    report.grouping_name_human
   end
 
   def header_past_months
-    @report.step_past_months { |date| l(date, format: :month) }
+    out = []
+    report.step_past_months { |date| out << l(date, format: :month) }
+    out
   end
 
   def header_past_months_summary
@@ -44,39 +48,95 @@ class Reports::Revenue::Csv
   end
 
   def header_future_months
-    report.step_future_months { |date| l(date, format: :month) }
+    out = []
+    report.step_future_months { |date| out << l(date, format: :month) }
+    out
   end
 
   def entries
     @entries ||=
-      if @report.hours_without_entry?
-        @report.entries + [nil]
+      if report.hours_without_entry?
+        report.entries + [nil]
       else
-        @report.entries
+        report.entries
       end
   end
 
+  # Rows
+
   def row
-    [row_month, row_past_months, row_past_months_summary]
+    [row_grouping, row_past_months, row_past_months_summary, row_future_months].flatten
   end
 
-  def row_month
+  def row_grouping
     entry.presence || "#{report.grouping_name_human} nicht zugewiesen"
   end
 
   def row_past_months
+    out = []
     report.step_past_months do |date|
-      format_number(report.ordertime_hours[[entry.try(:id), date]] || 0, 0)
+      out << format_number(report.ordertime_hours[[entry.try(:id), date]] || 0, 0)
     end
+    out
   end
 
   def row_past_months_summary
     if report.past_months?
       [
-        format_number(@report.total_ordertime_hours_per_entry(entry), 0),
-        format_number(@report.average_ordertime_hours_per_entry(entry), 0)
+        format_number(report.total_ordertime_hours_per_entry(entry), 0),
+        format_number(report.average_ordertime_hours_per_entry(entry), 0)
       ]
     end
   end
 
+  def row_future_months
+    out = []
+    report.step_future_months do |date|
+      out << format_number(report.planning_hours[[entry.try(:id), date]] || 0, 0)
+    end
+    out
+  end
+
+  # Footer
+
+  def footer
+    [footer_total, footer_past_months, footer_past_months_summary, footer_future_months].flatten
+  end
+
+  def footer_total
+    'Total'
+  end
+
+  def footer_past_months
+    out = []
+    report.step_past_months do |date|
+      out << format_number(report.total_ordertime_hours_per_month[date] || 0, 0)
+    end
+    out
+  end
+
+  def footer_past_months_summary
+    if report.past_months?
+      [
+        format_number(report.total_ordertime_hours_overall, 0),
+        format_number(report.average_ordertime_hours_overall, 0)
+      ]
+    end
+  end
+
+  def footer_future_months
+    out = []
+    report.step_future_months do |date|
+      out << format_number(report.total_planning_hours_per_month[date] || 0, 0)
+    end
+    out
+  end
+
+  def l(*args)
+    I18n.l(*args)
+  end
+
+  def format_number(number, precision = nil)
+    ActionController::Base.helpers.number_with_precision(number, precision: precision, delimiter: nil, separator: '.')
+  end
 end
