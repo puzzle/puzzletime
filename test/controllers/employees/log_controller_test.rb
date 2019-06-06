54071441212b90df190cdcdced9a249483add3a5
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-06-06 14:41:44 +0200

    Add committed worktime to employee log

--- test/controllers/employees/log_controller_test.rb
+++ test/controllers/employees/log_controller_test.rb
@@ -26,9 +26,12 @@ class Employees::LogControllerTest < ActionController::TestCase
   test 'renders log in correct order' do
     pedro.update_attributes(street: 'Belpstrasse 37', postal_code: '3007', city: 'Bern')
     pedro.update_attributes(phone_private: '+41791234567')
-    pedro.update_attributes(committed_worktimes_at: Time.zone.now) # should not appear in log
+    pedro.update_attributes(committed_worktimes_at: Date.new(2000, 01, 31))
+    pedro.update_attributes(eval_periods: ['5w']) # should not appear in log
     get :index, params: { id: pedro.id }
-    assert_select('.log tbody tr', count: 2)
+    assert_select('.log tbody tr', count: 3)
+    assert_select('.log tbody tr:nth-child(1) td:nth-child(2)', text: "Der Member #2 wurde bearbeitet.\n" \
+                                                                      'Letzte freigegebene Periode des Members wurde auf «Januar 2000» gesetzt.')
     assert_select('.log tbody tr:nth-child(1) td:nth-child(2)', text: "Der Member #2 wurde bearbeitet.\n" \
                                                                       'Telefon Privat des Members wurde auf «+41791234567» gesetzt.')
     assert_select('.log tbody tr:nth-child(2) td:nth-child(2)', text: "Der Member #2 wurde bearbeitet.\n" \
