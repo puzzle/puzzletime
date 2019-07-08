7cb8a4e5ab466b370191dfae62f1125305606c9f
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-07-08 15:02:33 +0200

    Remove old error pages and use custom controller

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module Puzzletime
                                 #{config.root}/app/domain
                                 #{config.root}/app/jobs)
 
+    # Use custom error controller
+    config.exceptions_app = self.routes
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
