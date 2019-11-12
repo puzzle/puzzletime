9f2b10505143181a69fd051cb015194d3a7463a7
Thomas Burkhalter <new.mysteryman@gmail.com>
2019-11-13 00:56:53 +0100

    Allow config with and without anonymous ldap access

--- app/models/util/ldap_authenticator.rb
+++ app/models/util/ldap_authenticator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #  Copyright (c) 2006-2017, Puzzle ITC GmbH. This file is part of
 #  PuzzleTime and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
@@ -5,41 +7,132 @@
 
 
 class LdapAuthenticator
+  attr_reader :username, :password
+
+  def initialize(username = nil, password = nil)
+    @username = username
+    @password = password
+  end
+
   # Performs a login over LDAP with the passed data.
   # Returns the logged-in Employee or nil if the login failed.
-  def login(username, pwd)
-    return nil if Settings.ldap.connection.host.blank?
+  def login(username = nil, password = nil)
+    @username = username if username
+    @password = password if password
+    return nil if config[:host].blank?
+    return unless valid_user?
 
-    if !username.strip.empty? &&
-       (auth_user(Settings.ldap.user_dn, username, pwd) ||
-        (auth_user(Settings.ldap.external_dn, username, pwd) && group_member(username)))
-      Employee.find_by(ldapname: username)
-    end
+    Employee.find_by(ldapname: @username)
   end
 
-  def auth_user(dn, username, pwd)
-    connection.bind_as(base: dn,
-                       filter: "uid=#{username}",
-                       password: pwd)
-  end
-
-  def group_member(username)
-    result = connection.search(base: Settings.ldap.group,
-                               filter: Net::LDAP::Filter.eq('memberUid', username))
-    !result.empty?
+  def auth_user(base_dn)
+    connection.bind_as(
+      base: base_dn,
+      filter: "uid=#{username}",
+      password: password
+    )
   end
 
   # Returns a Array of LDAP user information
   def all_users
-    connection.search(base: Settings.ldap.user_dn,
-                      attributes: %w(uid sn givenname mail))
+    connection.search(
+      base: internal_dn,
+      attributes: %w(uid sn givenname mail)
+    )
   end
 
   private
 
-  def connection
-    config = Settings.ldap.connection.to_hash
-    config[:encryption] = config[:encryption].to_sym if config[:encryption]
-    Net::LDAP.new(config)
+  def valid_user?
+    return if username.strip.empty?
+    return unless internal_user? || external_user?
+
+    true
   end
+
+  def internal_user?
+    auth_user(internal_dn)
+  end
+
+  def external_user?
+    auth_user(external_dn) && group_member?
+  end
+
+  def group_member?
+    group  = connection.search(base: group_dn, attributes: :member).first
+    member = group[:member]
+    member.any? { |m| m =~ /uid=#{username}/ }
+  end
+
+  def connection
+    @connection ||=
+      begin
+        con = Net::LDAP.new(config)
+        bindauth(con)
+        con
+      end
+  end
+
+  def bindauth(connection)
+    return unless binduser
+    return unless bindpassword
+
+    connection.auth binduser, bindpassword
+  end
+
+  def config
+    @config ||=
+      begin
+        cfg = Settings.ldap.connection.to_hash
+        cfg[:encryption][:method] = cfg[:encryption][:method].to_sym if cfg.dig(:encryption, :method)
+        cfg[:encryption][:tls_options] = tls_options
+        cfg
+      end
+  end
+
+  def tls_options
+    {
+      verify_mode: OpenSSL::SSL::VERIFY_PEER,
+      cert_store: cert_store
+    }
+  end
+
+  def internal_dn
+    @internal_dn ||= Settings.ldap.user_dn
+  end
+
+  def external_dn
+    @external_dn ||= Settings.ldap.external_dn
+  end
+
+  def group_dn
+    @group_dn ||= Settings.ldap.group
+  end
+
+  def binduser
+    @binduser ||= Settings.ldap.auth.binduser
+  end
+
+  def bindpassword
+    @bindpassword ||= Settings.ldap.auth.bindpassword
+  end
+
+  def root_cert
+    @root_cert ||= OpenSSL::X509::Certificate.new(Settings.ldap.cert.root)
+  end
+
+  def chain_cert
+    @chain_cert ||= OpenSSL::X509::Certificate.new(Settings.ldap.cert.chain)
+  end
+
+  def cert_store
+    @cert_store ||=
+      begin
+        store = OpenSSL::X509::Store.new
+        store.add_cert root_cert
+        store.add_cert chain_cert
+        store
+      end
+  end
+
 end
