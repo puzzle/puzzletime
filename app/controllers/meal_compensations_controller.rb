ad1a773ffa7b0a5e780e9a68af2c20a73c6a5a20
Andreas Zuber <zuber@puzzle.ch>
2020-04-03 17:01:07 +0200

    Add detail view to meal compensation evaluation

--- app/controllers/meal_compensations_controller.rb
+++ app/controllers/meal_compensations_controller.rb
@@ -23,6 +23,15 @@ class MealCompensationsController < ApplicationController
                           .where(meal_compensation: true)
   end
 
+  def details
+    @employee = Employee.find(params[:id])
+    @work_item = WorkItem.find(params[:work_item])
+    @worktimes = @employee.worktimes
+                          .in_period(@period)
+                          .where(meal_compensation: true)
+                          .where(work_item: @work_item)
+  end
+
   private
 
   def authorize_class
