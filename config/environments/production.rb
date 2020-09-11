094f6d645635fb27a1754dabfc1ea561d701d7d6
Thomas Burkhalter <new.mysteryman@gmail.com>
2020-09-11 17:05:18 +0200

    Fix ticket #130

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,9 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
-  config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = ENV['RAILS_X_SENDFILE_HEADER'] || 'X-Sendfile'
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
