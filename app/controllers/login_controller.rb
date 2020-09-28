96bad51487ba2f52a1fcf9515b07b1dfce441baa
psunix <psunix@s-it.ch>
2020-09-28 13:34:54 +0200

    Skip CSRF token check for POST login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,6 +7,8 @@
 
 class LoginController < ApplicationController
   skip_before_action :authenticate, except: [:logout]
+  # it's safe to ignore CSRF token for login
+  skip_before_action :verify_authenticity_token, only: :login
   skip_authorization_check
 
   def index
