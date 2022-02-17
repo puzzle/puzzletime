3dc604f1fa688f4ef2bcb32a9e8dd876635ffcef
Thomas Burkhalter <new.mysteryman@gmail.com>
2022-02-17 15:32:36 +0100

    Adhoc fixes to get this branch working

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,17 +1,19 @@
 # Be sure to restart your server when you modify this file.
 
-def dalli_reachable?
-  Rails.cache.dalli.stats.values.any?
+def cache_reachable?
+  Rails.cache.stats.values.any?
 end
 
 def memcache_configured?
   if Rails.env.production?
     ENV['RAILS_MEMCACHED_HOST'].present?
-  elsif Rails.env.development?
-    true
   else
-    false
+    Rails.env.development?
   end
+end
+
+def skip_memcache_check
+  ENV['SKIP_MEMCACHE_CHECK'].present?
 end
 
 secure_cookies = !(Rails.env.development? || Rails.env.test?)
@@ -21,7 +23,11 @@ Rails.application.config.session_store ActionDispatch::Session::CacheStore,
                                        same_site: :lax,
                                        secure: secure_cookies
 
-# We expect memcache to work in production. Prevents an error with the rails console on OpenShift
-if memcache_configured? && !Rails.env.production? && !dalli_reachable?
+# We expect memcache to work in production.
+# Prevents an error with the rails console on OpenShift
+if !skip_memcache_check &&
+   memcache_configured? &&
+   !Rails.env.production? &&
+   !cache_reachable?
   fail "As CSRF tokens are read from cache, we require a memcache instance to start"
 end
