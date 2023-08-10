a753462c64152a8d392730e7af2c6146b9d705e5
Daniel Illi <illi@puzzle.ch>
2023-08-10 15:34:17 +0200

    Add migration for rails 7, new config defaults

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require_relative 'version'
 module Puzzletime
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # FIXME: remove this if it works flawlesly
     config.active_record.belongs_to_required_by_default = false
---
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -0,0 +1,7 @@
+# New default is `OpenSSL::Digest::SHA256`, we still use the old value for now
+Rails.application.config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
+
+# Enable parameter wrapping for JSON.
+# Previously this was set in an initializer. It's fine to keep using that initializer if you've customized it.
+# To disable parameter wrapping entirely, set this config to `false`.
+# Rails.application.config.action_controller.wrap_parameters_by_default = true
