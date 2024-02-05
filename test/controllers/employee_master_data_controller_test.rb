1c454befc69b54e24fdde980d9b5b9d42b2996bc
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-02-06 00:26:00 +0100

    Remove leftover zxing require

--- test/controllers/employee_master_data_controller_test.rb
+++ test/controllers/employee_master_data_controller_test.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 #  Copyright (c) 2006-2017, Puzzle ITC GmbH. This file is part of
 #  PuzzleTime and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/puzzle/puzzletime.
 
 require 'test_helper'
-require 'zxing'
 
-class EmployeeMasterDataControllerTest < ActionController::TestCase
+class EmployeeMasterDataControllerTest < ActionDispatch::IntegrationTest
   def setup
     login
 
