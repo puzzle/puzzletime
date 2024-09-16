ec61ab28a7ddbe9bbe9b96266c9d9bd0e5b97c04
Thomas Burkhalter <new.mysteryman@gmail.com>
2024-09-16 22:54:27 +0200

    Add amount to order_service CSV-Export

--- app/controllers/concerns/worktimes_csv.rb
+++ app/controllers/concerns/worktimes_csv.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2006-2017, Puzzle ITC GmbH. This file is part of
+#  Copyright (c) 2006-2024, Puzzle ITC GmbH. This file is part of
 #  PuzzleTime and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/puzzle/puzzletime.
@@ -19,13 +19,14 @@ module WorktimesCsv
 
   def worktimes_csv(worktimes)
     CSV.generate do |csv|
-      csv << ['Datum', 'Stunden', 'Von Zeit', 'Bis Zeit', 'Reporttyp',
+      csv << ['Datum', 'Stunden', 'Von Zeit', 'Bis Zeit', 'Stundenansatz CHF', 'Reporttyp',
               'Verrechenbar', 'Member', 'Position', 'Ticket', 'Bemerkungen']
       worktimes.each do |time|
         csv << [I18n.l(time.work_date),
                 time.hours,
                 (time.start_stop? ? I18n.l(time.from_start_time, format: :time) : ''),
                 (time.start_stop? && time.to_end_time? ? I18n.l(time.to_end_time, format: :time) : ''),
+                currency(time.amount),
                 time.report_type,
                 time.billable,
                 time.employee.label,
@@ -34,5 +35,12 @@ module WorktimesCsv
                 time.description]
       end
     end
+  end
+
+  def currency(value)
+    format(
+      '%<amount>0.02f',
+      amount: value,
+    )
   end
 end
