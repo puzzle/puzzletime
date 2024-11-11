1f507f07c7b6bd4af7781b110d2f77102a7bc9da
Reto Galante <galante@puzzle.ch>
2024-11-11 14:54:37 +0100

    build and lint using dagger

---
+++ ci/main.go
@@ -0,0 +1,39 @@
+// A generated module for Ci functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"dagger/ci/internal/dagger"
+)
+
+type Ci struct{}
+
+// Returns a Container with installed gems from the gemfile in the provided Directory
+func (m *Ci) Build(ctx context.Context, directory *dagger.Directory) *dagger.Container {
+	return dag.Container().
+		From("ruby:latest").
+		WithMountedDirectory("/mnt", directory).
+		WithWorkdir("/mnt").
+		WithExec([]string{"gem", "install", "rails"}).
+        WithExec([]string{"bundle", "config", "bundle", "install"})
+}
+
+func (m *Ci) Lint(ctx context.Context, directory *dagger.Directory) *dagger.Container {
+    return m.Build(ctx, directory).
+		WithExec([]string{"gem", "install", "haml-lint"}).
+		WithExec([]string{"haml-lint"})
+		//WithExec([]string{"haml-lint", "|", "reviewdog", "-efm=\"%f:%l %m\"", "-name=\"HAML-Lint\"", "-reporter=github-pr-review", "-level=error", "-diff=\"git diff\""})
+}
