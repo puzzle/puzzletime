638c38b68f46d3c61bcb75452a8807f774a294fa
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-02-26 02:04:50 +0100

    WIP: First version of ODOO API

--- app/domain/crm/odoo/partner.rb
+++ app/domain/crm/odoo/partner.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 module Crm
-  module Odoo
+  class Odoo
     class Partner < Base
       self.model = 'res.partner'
       self.parameters = [['is_company', '=', false]].freeze
+
+      # This is sadly needed, since you can't filter on read calls
+      def self.resource(...)
+        result = super(...)
+
+       return result unless result["is_company"]
+      end
     end
   end
 end
