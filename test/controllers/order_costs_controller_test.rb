4afe4ee3cec09d3e4742f903baf0b7ab4b305b30
Sven Wey <wey@puzzle.ch>
2025-02-27 13:39:56 +0100

    add basic functionality for a new tab "costs"

---
+++ test/controllers/order_costs_controller_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2006-2017, Puzzle ITC GmbH. This file is part of
+#  PuzzleTime and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/puzzle/puzzletime.
+
+require 'test_helper'
+
+class OrderCostsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
