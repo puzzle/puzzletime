bea4b4520756a70e82d7ae81e878f8c4bedb7fcd
Sven Wey <wey@puzzle.ch>
2025-03-21 17:30:00 +0100

    add header, footer and order information components to report

--- app/controllers/order_services_controller.rb
+++ app/controllers/order_services_controller.rb
@@ -49,7 +49,7 @@ class OrderServicesController < ApplicationController
         render_report(list_worktimes(period))
       end
       format.pdf do
-        pdf_generator = Order::Services::TimeRapportPdfGenerator.new(@order, list_worktimes(period), params)
+        pdf_generator = Order::Services::TimeRapportPdfGenerator.new(@order, list_worktimes(period), @work_items, params)
 
         send_data pdf_generator.generate_pdf.render,
                   filename: 'report.pdf',
