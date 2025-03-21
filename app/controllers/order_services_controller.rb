fa1fcd6dd0d6cc983d975a4a6577d4f7cfe0f39c
Sven Wey <wey@puzzle.ch>
2025-03-21 16:36:05 +0100

    fix bug where in order_services upon no selection of an accounting_post there were no entries in the time_rapport

--- app/controllers/order_services_controller.rb
+++ app/controllers/order_services_controller.rb
@@ -75,8 +75,8 @@ class OrderServicesController < ApplicationController
   end
 
   def prepare_report_header
-    params[:work_item_ids] ||= [params[:work_item_id].presence || order.work_item_id]
-    @work_items = WorkItem.find(params[:work_item_ids])
+    work_item_ids = [params[:work_item_id].presence || order.work_item_id]
+    @work_items = WorkItem.find(work_item_ids)
     @employee = Employee.find(params[:employee_id]) if params[:employee_id].present?
     set_period_with_invoice
   end
