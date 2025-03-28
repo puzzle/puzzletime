c8e10c9349c50c396993574bee4f55849c85539c
Sven Wey <wey@puzzle.ch>
2025-03-28 11:51:26 +0100

    pass struct with params instead of passing all of them directly for better clarity. Disable rubocop for worktimes_table_rows, as it is readable enough

--- app/controllers/evaluator_controller.rb
+++ app/controllers/evaluator_controller.rb
@@ -46,7 +46,16 @@ class EvaluatorController < ApplicationController
     prepare_report_header
     conditions = params[:only_billable] ? { worktimes: { billable: true } } : {}
     prepare_worktimes(@evaluation.times(@period).includes(:work_item).where(conditions))
-    pdf_generator = Order::Services::TimeRapportPdfGenerator.new(@order, @worktimes, @tickets, @ticket_view, @employees, @employee, @work_items, @period, params)
+    time_rapport_data = Order::Services::TimeRapportData.new(order: @order, 
+                                                             worktimes: @worktimes, 
+                                                             tickets: @tickets, 
+                                                             ticket_view: @ticket_view, 
+                                                             employees: @employees, 
+                                                             employee: @employee, 
+                                                             work_items: @work_items, 
+                                                             period: @period
+                                                             )
+    pdf_generator = Order::Services::TimeRapportPdfGenerator.new(time_rapport_data, params)
 
     send_data pdf_generator.generate_pdf.render,
               filename: 'report.pdf',
