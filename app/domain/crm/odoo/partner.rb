f0a8e19aba84e04e8e93526ffabf98bc581f0c3c
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-04-17 17:28:06 +0200

    Improve prefetching

--- app/domain/crm/odoo/partner.rb
+++ app/domain/crm/odoo/partner.rb
@@ -5,7 +5,7 @@ module Crm
     class Partner < Base
       self.model = 'res.partner'
       self.parameters = [['is_company', '=', false]].freeze
-      self.options = {fields: %i[id name function email_normalized phone mobile]}.freeze
+      self.options = {fields: %i[id parent_id name function email_normalized phone mobile]}.freeze
 
       # This is sadly needed, since you can't filter on read calls
       def self.resource(...)
