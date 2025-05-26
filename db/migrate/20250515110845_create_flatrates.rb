312e5e526a26b93dea20cfda5929ae14085796d0
Sven Wey <wey@puzzle.ch>
2025-05-26 12:41:16 +0200

    add support for flatrates in accounting posts (wip)

---
+++ db/migrate/20250515110845_create_flatrates.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateFlatrates < ActiveRecord::Migration[7.1]
+  def change
+    create_table :flatrates do |t|
+      t.string :name
+      t.boolean :active, default: true, null: false
+      t.text :description
+      t.decimal :amount
+      t.integer :periodicity, array: true, default: []
+      t.references :accounting_post, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
