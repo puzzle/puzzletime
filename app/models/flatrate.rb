ca750c945ecb56ac53e60898d580bb2b7a660ef6
Sven Wey <wey@puzzle.ch>
2025-05-28 10:17:54 +0200

    make periodicity a list of integers which represents the quantity of flatrates billed after every specific month

--- app/models/flatrate.rb
+++ app/models/flatrate.rb
@@ -7,12 +7,26 @@ class Flatrate < ApplicationRecord
     "#{name} (#{accounting_post.name})"
   end
 
+  (0..11).each do |i|
+    define_method(:"periodicity_#{i}") do
+      periodicity[i].to_i
+    rescue StandardError
+      0
+    end
+
+    define_method(:"periodicity_#{i}=") do |val|
+      current = periodicity.dup
+      current[i] = val.to_i
+      self.periodicity = current # triggers ActiveRecord change tracking
+    end
+  end
+
+  # Ensure periodicity is always an array of 12 integers
   def periodicity
-    super&.map(&:to_s) || []
+    super&.map(&:to_i)&.fill(0, super.size...12) || Array.new(12, 0)
   end
 
   def periodicity=(vals)
-    cleaned = Array(vals).compact_blank
-    super(cleaned.map(&:to_i))
+    super(Array(vals).map(&:to_i).fill(0, vals.size...12))
   end
 end
