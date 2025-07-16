eb5e57f4719a19a49b3fdbd0b1b18240078d7982
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-07-16 09:23:09 +0200

    Use correct Time method

--- app/domain/order/services/time_rapport_pdf_generator.rb
+++ app/domain/order/services/time_rapport_pdf_generator.rb
@@ -69,7 +69,7 @@ class Order
         customer_data << ['Member', @employee.label] if @employee
         customer_data << ['Periode', @period.to_s]
         customer_data << ['Verrechenbar', params[:billable].present? ? I18n.t("global.#{params[:billable]}") : 'Alle']
-        customer_data << ['Generiert am', "#{Time.zone.now.strftime('%d.%m.%Y, %H:%M')} Uhr"]
+        customer_data << ['Generiert am', "#{Time.now.strftime('%d.%m.%Y, %H:%M')} Uhr"]
 
         # Draw customer/order/time info as a table
         pdf.text "Zeitrapport #{Company.name}", size: 18, style: :bold
