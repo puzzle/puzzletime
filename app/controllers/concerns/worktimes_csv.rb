eb2cec71c26bc3eb5a95bd263dd1da9894612c9b
Linus Degen <degen@puzzle.ch>
2025-12-23 09:06:53 +0100

    Export worktime csv without internal description. refs #64960

--- app/controllers/concerns/worktimes_csv.rb
+++ app/controllers/concerns/worktimes_csv.rb
@@ -12,31 +12,37 @@ module WorktimesCsv
 
   private
 
-  def send_worktimes_csv(worktimes, filename)
-    csv_data = worktimes_csv(worktimes)
+  # @param stripped: include/exclude internal description in csv
+  def send_worktimes_csv(worktimes, filename, stripped = false)
+    csv_data = CSV.generate do |csv|
+      csv << header(stripped)
+
+      worktimes.each do |time|
+        csv << row(time, stripped)
+      end
+    end
+
     send_csv(csv_data, filename)
   end
 
-  def worktimes_csv(worktimes)
-    CSV.generate do |csv|
-      csv << ['Datum', 'Stunden', 'Von Zeit', 'Bis Zeit', 'CHF', 'Stundenansatz CHF', 'Reporttyp',
-              'Verrechenbar', 'Member', 'Position', 'Ticket', 'Bemerkungen', 'Interne Bemerkungen']
-      worktimes.each do |time|
-        csv << [I18n.l(time.work_date),
-                time.hours,
-                (time.start_stop? ? I18n.l(time.from_start_time, format: :time) : ''),
-                (time.start_stop? && time.to_end_time? ? I18n.l(time.to_end_time, format: :time) : ''),
-                amount(time),
-                offered_rate(time),
-                time.report_type,
-                time.billable,
-                time.employee.label,
-                time.account.label_verbose,
-                time.ticket,
-                time.description,
-                time.internal_description]
-      end
+  def header(stripped = false)
+    header = ['Datum', 'Stunden', 'Von Zeit', 'Bis Zeit', 'CHF', 'Stundenansatz CHF', 'Reporttyp',
+     'Verrechenbar', 'Member', 'Position', 'Ticket', 'Bemerkungen']
+    unless stripped
+      header << 'Interne Bemerkungen'
     end
+    header
+  end
+
+  def row(e, stripped = false)
+    data = [I18n.l(e.work_date), e.hours, (e.start_stop? ? I18n.l(e.from_start_time, format: :time) : ''),
+     (e.start_stop? && e.to_end_time? ? I18n.l(e.to_end_time, format: :time) : ''),
+     amount(e), offered_rate(e), e.report_type, e.billable, e.employee.label, e.account.label_verbose,
+     e.ticket, e.description]
+    unless stripped
+      data << e.internal_description
+    end
+    data
   end
 
   def offered_rate(time)
