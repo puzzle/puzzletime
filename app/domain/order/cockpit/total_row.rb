a8be5f2d31e7075240953579033ffde43239d9df
Thomas Burkhalter <new.mysteryman@gmail.com>
2026-03-24 16:21:38 +0100

    Fix accounting post total rows link (64388)

--- app/domain/order/cockpit/total_row.rb
+++ app/domain/order/cockpit/total_row.rb
@@ -25,7 +25,8 @@ class Order
         master&.each_key do |key|
           hash[key] =
             Cell.new(sum_non_nil_values(cells, key, :hours, :to_d),
-                     sum_non_nil_values(cells, key, :amount, :to_d))
+                     sum_non_nil_values(cells, key, :amount, :to_d),
+                     link_path(key))
         end
         hash
       end
@@ -43,6 +44,28 @@ class Order
         return if values.all?(&:nil?)
 
         values.sum(&converter)
+      end
+
+      def link_path(key)
+        case key
+        when :supplied_services
+          order_order_services_path(
+            @order.id,
+            link_options
+          )
+        when :not_billable
+          order_order_services_path(
+            @order.id,
+            link_options.merge(billable: false)
+          )
+        end
+      end
+
+      def link_options
+        {
+          start_date: @period.start_date,
+          end_date: @period.end_date
+        }
       end
     end
   end
