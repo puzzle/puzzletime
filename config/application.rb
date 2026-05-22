fef50613e8c31ccbde520097285c1397560c2a20
Linus Degen <degen@puzzle.ch>
2026-05-22 14:32:15 +0200

    build(rails): upgrade from 7.1 to 7.2
    - Bump rails to ~> 7.2.0, update load_defaults to 7.2
    - Switch activerecord-nulldb-adapter from puzzle/nulldb fork to
      official gem 1.2.2 (fork's unique_constraint patch is now upstream,
      and 1.2.2 supports activerecord >= 6.1, < 8.2)
    - Replace deprecated ActiveRecord::Base.connection with with_connection
      in StatusController#can_query_database?
    - Fix captionize in format_helper to guard against blank attribute names
      (Rails 7.2 changed human_attribute_name behaviour for enum models)
    - Fix dry_crud form control to use plain label tag for blank attributes,
      bypassing the Rails label helper's internal human_attribute_name call

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require_relative 'version'
 module Puzzletime
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # FIXME: remove this if it works flawlesly
     config.active_record.belongs_to_required_by_default = false
